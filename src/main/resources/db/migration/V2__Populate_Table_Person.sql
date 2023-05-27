2855ea1f2131445954ac17a4a8994d17de066d03
Abner <rick.santos123@hotmail.com>
2023-05-26 21:18:30 -0300

    feat: migrations with flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,14 @@
+INSERT INTO `person` (id, first_name, last_name, address, gender) VALUES
+  ('1', 'Abner Henrique', 'dos Santos Simas', 'Rua Itororó, 410', 'Male'),
+  ('2', 'Joao','Simas', 'Alameda, 103', 'Male'),
+  ('3', 'Beatriz','Cristina', 'Maria Assunta, 150', 'Female'),
+	('4', 'John', 'Doe', '123 Main St', 'Male'),
+	('5', 'Jane', 'Smith', '456 Elm St', 'Female'),
+	('6', 'Michael', 'Johnson', '789 Oak Ave', 'Male'),
+	('7', 'Emily', 'Williams', '987 Pine Ln', 'Female'),
+	('8', 'David', 'Brown', '654 Maple Dr', 'Male'),
+	('9', 'Sarah', 'Davis', '321 Cedar Rd', 'Female'),
+	('10', 'Christopher', 'Miller', '555 Birch Ave', 'Male'),
+	('11', 'Amanda', 'Anderson', '888 Walnut St', 'Female'),
+	('12', 'Matthew', 'Thompson', '222 Oakwood Dr', 'Male'),
+	('13', 'Olivia', 'White', '999 Pine St', 'Female');
