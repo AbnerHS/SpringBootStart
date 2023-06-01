ed88d99cced2a7f377d1d6cde0ce90e8cd50ce15
Abner <rick.santos123@hotmail.com>
2023-05-31 22:08:41 -0300

    test: BookService works

---
+++ src/test/java/br/com/abner/springbootstart/unittests/mockito/services/BookServicesTest.java
@@ -0,0 +1,181 @@
+package br.com.abner.springbootstart.unittests.mockito.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.abner.springbootstart.data.vo.v1.BookVO;
+import br.com.abner.springbootstart.exceptions.RequiredObjectIsNullException;
+import br.com.abner.springbootstart.model.Book;
+import br.com.abner.springbootstart.repository.BookRepository;
+import br.com.abner.springbootstart.services.BookServices;
+import br.com.abner.springbootstart.unittests.mapper.mocks.MockBook;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+public class BookServicesTest {
+
+    MockBook input;
+
+    @InjectMocks
+    private BookServices service;
+
+    @Mock
+    BookRepository repository;
+
+
+    @BeforeEach
+    void setUpMocks() throws Exception {
+        input = new MockBook();
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testFindAll() {
+        List<Book> bookList = input.mockEntityList();
+        
+        when(repository.findAll()).thenReturn(bookList);
+
+        List<BookVO> result = service.findAll();
+        assertNotNull(result);
+        assertEquals(14, bookList.size());
+
+        BookVO bookOne = result.get(1);
+        
+        assertNotNull(bookOne);
+        assertNotNull(bookOne.getId());
+        assertNotNull(bookOne.getLinks());
+        assertTrue(bookOne.toString().contains("links: [</api/book/v1/1>;rel=\"self\"]"));
+        assertEquals("Author Test1", bookOne.getAuthor());
+        assertEquals("Title Test1", bookOne.getTitle());
+        assertEquals(5D, bookOne.getPrice());
+        assertNotNull(bookOne.getLaunchDate());
+
+        BookVO bookFive = result.get(5);
+        
+        assertNotNull(bookFive);
+        assertNotNull(bookFive.getId());
+        assertNotNull(bookFive.getLinks());
+
+        assertTrue(bookFive.toString().contains("links: [</api/book/v1/5>;rel=\"self\"]"));
+        assertEquals("Author Test5", bookFive.getAuthor());
+        assertEquals("Title Test5", bookFive.getTitle());
+        assertEquals(25D, bookFive.getPrice());
+        assertNotNull(bookFive.getLaunchDate());
+    }
+
+    @Test
+    void testFindById() {
+        Book book = input.mockEntity(1);
+        book.setId(1L);
+
+        when(repository.findById(1L)).thenReturn(Optional.of(book));
+
+        BookVO result = service.findById(1L);
+        assertNotNull(result);
+        assertNotNull(result.getId());
+        assertNotNull(result.getLinks());
+        assertTrue(result.toString().contains("links: [</api/book/v1/1>;rel=\"self\"]"));
+        assertEquals("Author Test1", result.getAuthor());
+        assertEquals("Title Test1", result.getTitle());
+        assertEquals(5D, result.getPrice());
+        assertNotNull(result.getLaunchDate());
+    }
+
+    @Test
+    void testCreate() {
+        Book entity = input.mockEntity(1);
+        
+        Book persisted = entity;
+        persisted.setId(1L);
+
+        BookVO vo = input.mockVO(1);
+        vo.setId(1L);
+        when(repository.save(entity)).thenReturn(persisted);
+        
+        BookVO result = service.create(vo);
+
+        assertNotNull(result);
+        assertNotNull(result.getId());
+        assertNotNull(result.getLinks());
+        assertTrue(result.toString().contains("links: [</api/book/v1/1>;rel=\"self\"]"));
+        assertEquals("Author Test1", result.getAuthor());
+        assertEquals("Title Test1", result.getTitle());
+        assertEquals(5D, result.getPrice());
+        assertNotNull(result.getLaunchDate());
+    }
+
+    @Test
+    void testCreateWithNullBook() {
+        Exception exception = assertThrows(RequiredObjectIsNullException.class, () -> {
+            service.create(null);
+        });
+
+        String expectedMessage = "It is not allowed to persist a null object!";
+        String actualMessage = exception.getMessage();
+        
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void testUpdate() {
+        Book entity = input.mockEntity(1);
+        
+        Book persisted = entity;
+        persisted.setId(1L);
+
+        BookVO vo = input.mockVO(1);
+        vo.setId(1L);
+
+        when(repository.findById(1L)).thenReturn(Optional.of(entity));
+        when(repository.save(entity)).thenReturn(persisted);
+        
+        BookVO result = service.update(vo);
+
+        assertNotNull(result);
+        assertNotNull(result.getId());
+        assertNotNull(result.getLinks());
+        assertTrue(result.toString().contains("links: [</api/book/v1/1>;rel=\"self\"]"));
+        assertEquals("Author Test1", result.getAuthor());
+        assertEquals("Title Test1", result.getTitle());
+        assertEquals(5D, result.getPrice());
+        assertNotNull(result.getLaunchDate());
+    }
+
+    @Test
+    void testUpdateWithNullBook() {
+        Exception exception = assertThrows(RequiredObjectIsNullException.class, () -> {
+            service.update(null);
+        });
+
+        String expectedMessage = "It is not allowed to persist a null object!";
+        String actualMessage = exception.getMessage();
+        
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void testDelete() {
+        Book book = input.mockEntity(1);
+        book.setId(1L);
+
+        when(repository.findById(1L)).thenReturn(Optional.of(book));
+
+        service.delete(1L);
+    }
+}
