cfbac580d368e2802b695ebdfcec1d2e477700b9
Abner <rick.santos123@hotmail.com>
2023-06-01 21:03:49 -0300

    refactor: dependencies order

--- src/main/java/br/com/abner/springbootstart/config/OpenApiConfig.java
+++ src/main/java/br/com/abner/springbootstart/config/OpenApiConfig.java
@@ -9,9 +9,9 @@ import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 public class OpenApiConfig {
-    
+
     @Bean
-    public OpenAPI customOpenAPI() {
+    OpenAPI customOpenAPI() {
         return new OpenAPI()
             .info(new Info()
                 .title("RESTful API with Java 18 and Spring Boot 3.1")
