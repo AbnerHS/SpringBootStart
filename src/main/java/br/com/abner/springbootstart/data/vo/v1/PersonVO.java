ce6aae478ac54e4e9d55adb73f0d0f8bfb15102d
Abner <rick.santos123@hotmail.com>
2023-06-14 20:29:47 -0300

    feat: add "enabled" field to Person

--- src/main/java/br/com/abner/springbootstart/data/vo/v1/PersonVO.java
+++ src/main/java/br/com/abner/springbootstart/data/vo/v1/PersonVO.java
@@ -3,7 +3,6 @@ package br.com.abner.springbootstart.data.vo.v1;
 import java.io.Serializable;
 
 import org.springframework.hateoas.RepresentationModel;
-import org.springframework.lang.Nullable;
 
 public class PersonVO extends RepresentationModel<PersonVO> implements Serializable {
 
@@ -14,6 +13,7 @@ public class PersonVO extends RepresentationModel<PersonVO> implements Serializa
     private String lastName;
     private String address;
     private String gender;
+    private Boolean enabled;
 
     public PersonVO() {
     }
@@ -66,19 +66,33 @@ public class PersonVO extends RepresentationModel<PersonVO> implements Serializa
         this.gender = gender;
     }
 
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
-        int result = 1;
+        int result = super.hashCode();
         result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+        result = prime * result + ((address == null) ? 0 : address.hashCode());
+        result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+        result = prime * result + ((enabled == null) ? 0 : enabled.hashCode());
         return result;
     }
 
     @Override
-    public boolean equals(@Nullable Object obj) {
+    @SuppressWarnings("null")
+    public boolean equals(Object obj) {
         if (this == obj)
             return true;
-        if (obj == null)
+        if (!super.equals(obj))
             return false;
         if (getClass() != obj.getClass())
             return false;
@@ -88,7 +102,33 @@ public class PersonVO extends RepresentationModel<PersonVO> implements Serializa
                 return false;
         } else if (!id.equals(other.id))
             return false;
+        if (firstName == null) {
+            if (other.firstName != null)
+                return false;
+        } else if (!firstName.equals(other.firstName))
+            return false;
+        if (lastName == null) {
+            if (other.lastName != null)
+                return false;
+        } else if (!lastName.equals(other.lastName))
+            return false;
+        if (address == null) {
+            if (other.address != null)
+                return false;
+        } else if (!address.equals(other.address))
+            return false;
+        if (gender == null) {
+            if (other.gender != null)
+                return false;
+        } else if (!gender.equals(other.gender))
+            return false;
+        if (enabled == null) {
+            if (other.enabled != null)
+                return false;
+        } else if (!enabled.equals(other.enabled))
+            return false;
         return true;
     }
+   
 
 }
